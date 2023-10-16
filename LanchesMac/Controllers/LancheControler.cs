eace562f0f810f08afbdc82f3078c874f3a7b669
Jailton Lima <jailtonlima1001@gmail.com>
2023-10-15 22:39:54 -0300

    primeiro commit

---
+++ LanchesMac/Controllers/LancheControler.cs
@@ -0,0 +1,20 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheControler : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheControler(ILancheRepository lancheRepository) 
+        { 
+            _lancheRepository = lancheRepository;
+        }
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
